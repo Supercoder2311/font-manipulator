4e0aed891e4d0f6efed8f66000495fbe0d116bf4
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2023-01-06 19:55:09 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX =0;
+noseY =0;
+difference =0;
+leftWristX =0;
+rightWristX =0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -6,14 +12,18 @@ function setup(){
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('poses', gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
     background('#969A97');
+    document.getElementById("square_side").innerHTML = " Width and Height of a square will be =" +difference +" px";
+    fill('red');
+    stroke('blue');
+    square(noseX, noseY, difference);
 }
 
-function modelLoaded(results){
+function modelLoaded(){
     console.log('posenet is intialized!');
 }
 
@@ -21,5 +31,14 @@ function modelLoaded(results){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log(" noseX = " +noseX +" noseY =" +noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log(" leftWrist ="+ leftWristX +" rightWrist =" +rightWristX +" difference ="+ difference);
     }
 }
